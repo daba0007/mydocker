6ef8e1328db51e5d4e72d5214aecca916591361f
大巴0007 <yld060631@163.com>
2018-01-11 01:30:22 +0800

    添加写入redis及redis读取

--- web/dabaweb/form/views.py
+++ web/dabaweb/form/views.py
@@ -2,14 +2,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from form.models import User,User_data,Code_data
+from django.core.cache import cache
+
+def get_readed_cache(key):
+    if cache.has_key(key):
+        return cache.get(key)
+    else:
+        return key
 
 def home(request):
     return render(request, 'home.html')
 
 def user_data(request):
-    User_datalist = User_data.objects.all()
+    if get_readed_cache("User_datalist") == "User_datalist":
+        User_datalist = User_data.objects.all()
+    else:
+        User_datalist= get_readed_cache("User_datalist")
+        cache.set(key, User_datalist, 60 * 60)  # 设置有效期一小时,设置缓存
     return render(request, 'user_data.html',{'User_datalist':User_datalist})
 
 def code_data(request):
-    Code_datalist = Code_data.objects.all()
+    if get_readed_cache("Code_datalist") == "Code_datalist":
+        Code_datalist = Code_data.objects.all()
+    else:
+        Code_datalist= get_readed_cache("Code_datalist")
+        cache.set(key, Code_datalist, 60 * 60)  # 设置有效期一小时,设置缓存
     return render(request, 'code_data.html',{'Code_datalist':Code_datalist})
