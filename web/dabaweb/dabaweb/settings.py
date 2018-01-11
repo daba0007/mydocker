0807542bfd80a41a1639d79855d23dbc7b0f87be
大巴0007 <yld060631@163.com>
2018-01-11 12:20:47 +0800

    修改setting

--- web/dabaweb/dabaweb/settings.py
+++ web/dabaweb/dabaweb/settings.py
@@ -91,8 +91,15 @@ DATABASES = {
     }
 
 }
-
-
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
